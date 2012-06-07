a256697a1d05eb667e971ba52ae2d08da490370f
Vivek Galatage <vivekgalatage@gmail.com>
2012-06-07 21:40:43 +0530

    Cleaning up some testing

--- scripts/generate-metainfo.pl
+++ scripts/generate-metainfo.pl
@@ -1,5 +1,2 @@
 # TODO: Delete this parking line once done.
 # Perl file to auto-generate lots n lots code. Stay tuned.
-# Hello All... Go through the project... have Fun
-# Good start...
-# Saying hello from Lokesh.
