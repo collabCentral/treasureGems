2de621259a38ecd5a3b6f08627682aab43a97e5c
Vivek Galatage <vivekgalatage@gmail.com>
2012-12-14 00:10:49 +0530

    C++ conversion operator example

---
+++ src/cpp/ConvertionOperator.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class SmartPtr
+{
+public:
+    SmartPtr(T* ptr) : m_ptr(ptr) {}
+
+    operator T*() { return m_ptr; }
+    operator T&() { return *m_ptr; }
+
+private:
+    T* m_ptr;
+};
+
+class A
+{
+public:
+    void display() const { cout << "Hello World\n"; }
+    A() { cout <<"constructor\n"; }
+    A(const A&) { cout <<"copy constructor\n"; }
+    A& operator=(const A&) { cout <<"assignment\n"; return *this; }
+    ~A() { cout <<"destructor\n"; }
+
+};
+
+void hello1(const A& obj)
+{
+    obj.display();
+}
+
+void hello2(const A obj)
+{
+    obj.display();
+}
+
+void hello3(const A* ptr)
+{
+    ptr->display();
+}
+
+int main()
+{
+    SmartPtr<A> m(new A());
+    hello1(m);
+    hello2(m);
+    hello3(m);
+    return 0;
+}
+
